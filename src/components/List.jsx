223253eedac46e623fb8ad00c4e16b34db249ff5
yungan9 <geyun105@naver.com>
2024-09-13 05:21:26 +0900

    Feat:List Drag and Drop

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,21 +1,49 @@
-import React from 'react';
-import '../style/List.css';
+import React from "react";
+import "../style/List.css";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
-export default function List({todoData, setTodoData}) {
-    const handleClick = (id) =>{
-        let newTodoData = todoData.filter(data => data.id !== id);
-        console.log('new', newTodoData);
+export default function List({ todoData, setTodoData }) {
+    const handleDragEnd = (result) => {
+        if (!result.destination) return;
+    
+        const newTodoData = Array.from(todoData);
+        const [reorderedItem] = newTodoData.splice(result.source.index, 1);
+        newTodoData.splice(result.destination.index, 0, reorderedItem);
+    
         setTodoData(newTodoData);
-        }
+      };
 
-    return (
+  const handleClick = (id) => {
+    let newTodoData = todoData.filter((data) => data.id !== id);
+    console.log("new", newTodoData);
+    setTodoData(newTodoData);
+  };
+
+  return (
     <div>
-        {todoData.map(data => (
-            <div className='memoBlock' key={data.id}>
-                {data.title}
-                <button onClick={() => handleClick(data.id)}>x</button>
+      <DragDropContext onDragEnd={handleDragEnd}>
+        <Droppable droppableId="todo">
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {todoData.map((data, index) => (
+                <Draggable
+                  key={data.id}
+                  draggableId={data.id.toString()}
+                  index={index}
+                >
+                  {(provided,snapshot) => (
+                    <div className="memoBlock" key={data.id} {...provided.draggableProps} ref={provided.innerRef} {...provided.dragHandleProps}>
+                      {data.title}
+                      <button onClick={() => handleClick(data.id)}>x</button>
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
             </div>
-            ))}
-        </div>
-    )
+          )}
+        </Droppable>
+      </DragDropContext>
+    </div>
+  );
 }

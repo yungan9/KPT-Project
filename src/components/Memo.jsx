e27f20509a4ff49195edaec0aa468737986d36b8
yungan9 <geyun105@naver.com>
2024-09-13 06:00:33 +0900

    Feat:memoBlock contents Update

--- src/components/Memo.jsx
+++ src/components/Memo.jsx
@@ -1,23 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function Memo({id, title, todoData, setTodoData, provided, snapshot}) {
+export default function Memo({
+  id,
+  title,
+  todoData,
+  setTodoData,
+  provided,
+}) {
   const handleClick = (id) => {
     let newTodoData = todoData.filter((data) => data.id !== id);
     console.log("new", newTodoData);
     setTodoData(newTodoData);
   };
-  return (
-    <div>
-      <div
-        className="memoBlock"
-        key={id}
-        {...provided.draggableProps}
-        ref={provided.innerRef}
-        {...provided.dragHandleProps}
-      >
-        {title}
-        <button onClick={() => handleClick(id)}>x</button>
+
+  const handleEditChange = (event) => {
+    setEdited(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    let newTodoData = todoData.map((data) => {
+      if (data.id === id) {
+        data.title = edited;
+      }
+      return data;
+    });
+    setTodoData(newTodoData);
+    setIsEditing(false);
+  };
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [edited, setEdited] = useState(title);
+
+  if (isEditing) {
+    return (
+      <div>
+        <div className="memoBlock">
+          <form onSubmit={handleSubmit}>
+            <input value={edited} onChange={handleEditChange} />
+            <button type="submit">save</button>
+            <button onClick={(handleSubmit) => setIsEditing(false)}>x</button>
+          </form>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div>
+        <div
+          className="memoBlock"
+          key={id}
+          {...provided.draggableProps}
+          ref={provided.innerRef}
+          {...provided.dragHandleProps}
+        >
+          {title}
+          <button onClick={() => setIsEditing(true)}>edit</button>
+          <button onClick={() => handleClick(id)}>x</button>
+        </div>
+      </div>
+    );
+  }
 }

e074a942b81b0a06439ebca11703696cb313644a
yungan9 <geyun105@naver.com>
2024-09-13 03:50:46 +0900

    Feat:memoBlock Delete

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import React, {Component} from 'react';
-
-const todoData =[
-  {
-    id:"1",
-    title:"공부하기",
-  },
-  {
-    id:"2",
-    title:"청소하기",
-  }
-];
-
+import React, {useState} from 'react';
 
 function App() {
+
+  const [todoData, setTodoDate] = useState( [
+    {
+      id:"1",
+      title:"공부하기",
+    },
+    {
+      id:"2",
+      title:"청소하기",
+    }
+  ]);
+  
+  const handleClick = (id) =>{
+    let newTodoData = todoData.filter(data => data.id !== id);
+    console.log('new', newTodoData);
+    setTodoDate(newTodoData);
+  }
   
   return (
     <div className='container'>
